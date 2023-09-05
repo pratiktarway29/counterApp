624df675c38a2e0e73695a2a4c5ca489aefa88c7
pratiktarway29 <twexaa@gmail.com>
2023-09-05 17:16:59 +0530

    Adding App

---
+++ src/CounterApp.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+const CounterApp = () => {
+    const [count, setCount] = useState(0);
+
+    const increment = () => {
+        setCount(count + 1);
+    };
+
+    const decrement = () => {
+        if (count > 0) {
+            setCount(count - 1);
+        }
+    };
+
+    return (
+        <div>
+            <h1>Counter App</h1>
+            <p>Count: {count}</p>
+            <button onClick={increment}>Increment</button>
+            <button onClick={decrement}>Decrement</button>
+        </div>
+    );
+};
+
+export default CounterApp;
